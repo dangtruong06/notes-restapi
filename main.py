ccfbcae3461a88321ce059396a513152a25409ee
dangtruong06 <DANGTT066@GMAIL.COM>
2026-07-25 16:19:19 -0700

    add update and delete routes, simple api is fully functional to add, update, delete, get

--- main.py
+++ main.py
@@ -23,5 +23,30 @@ def add_note():
     db.session.commit()
     return jsonify(new_note.to_dict()), 201
 
+@app.route('/api/notes/<int:note_id>', methods=['PUT'])
+def update_note(note_id):
+    note = db.session.get(Note, note_id)
+    
+    if note:
+        updated_content = request.get_json()
+        note.title = updated_content.get("title", note.title)
+        note.content = updated_content.get("content", note.content)
+        db.session.commit()
+
+        return jsonify(note.to_dict()), 200
+
+    return jsonify("Error: resource not found"), 404
+
+@app.route('/api/notes/<int:note_id>', methods=['DELETE'])
+def delete_note(note_id):
+    note = db.session.get(Note, note_id)
+
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+        return "", 204
+    
+    return jsonify("Error: resource not found"), 404
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
